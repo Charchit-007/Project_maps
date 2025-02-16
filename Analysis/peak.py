ab3035ae00baf1011d6be39c45cb981d184d92c6
pynuz <152525579+anuj219@users.noreply.github.com>
2025-02-16 21:14:51 +0530

    Update peak.py for line plots Anuj

--- Analysis/peak.py
+++ Analysis/peak.py
@@ -12,7 +12,7 @@ import seaborn as sns
 def peak_hour_func(street, street_data, street_acc):
     # Get hourly volumes and accidents
     
-    hourly_volumes = street_data.groupby('HH')['Vol'].mean().reset_index()
+    hourly_volumes = street_data.groupby('HH')['Vol'].sum().reset_index()
     hourly_accidents = street_acc.groupby('Hour').size().reset_index(name='Accidents')
 
     # Merge the data
@@ -22,13 +22,13 @@ def peak_hour_func(street, street_data, street_acc):
     # # Calculate risk ratio (accidents per volume)
     hourly_analysis['Risk Ratio'] = (hourly_analysis['Accidents'] / hourly_analysis['Average Volume']) * 1000  # per 1000 vehicles
 
-
+ 
     # # Find the riskiest hour
     riskiest_hour = hourly_analysis.sort_values('Risk Ratio', ascending=False).reset_index()
     print(riskiest_hour)
     # Plot risk ratio by hour
     plt.figure(figsize=(12, 6))
-    sns.barplot(data=hourly_analysis, x='Hour1', y='Risk Ratio')
+    sns.lineplot(data=hourly_analysis, x='Hour1', y='Risk Ratio')
     plt.axvline(x=int(riskiest_hour.loc[0, 'Hour1']), 
                 color='red', linestyle='--', 
                 label=f"Riskiest Hour: {int(riskiest_hour.loc[0, 'Hour1'])}:00")
