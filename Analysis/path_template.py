3a31500ada22aaabbd5bdc99d7509606002f80e9
aman-sharma14 <sharma.aman.14@proton.me>
2025-02-18 22:54:50 +0530

    changed dashboard ui. added path and path_template constants file

---
+++ Analysis/path_template.py
@@ -0,0 +1,5 @@
+VOLUME_DATA_PATH = r"" #raw dataset csv
+TRAFFIC_MODEL_PATH = r"" #machine learning model pkl
+VOLUME_DATASET_PATH = r""  # Cleaned data with weather csv
+ACCIDENT_DATA_PATH = r"" #nyc accidents dataset csv
+SPEEDS_DATA_PATH = r"" #speed dataset json
