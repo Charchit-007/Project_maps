43ede65b4c0bbcd8ce9bc61b58d063549a988c9f
aman-sharma14 <sharma.aman.14@proton.me>
2025-02-20 20:55:07 +0530

    fixed navigation

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,10 @@ class OpenStreetMapRouteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MapsPage(),
+      home: const MapsPage(),
+     
     );
   }
 }
